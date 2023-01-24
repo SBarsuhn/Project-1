6b53a2bcc0c5167fd7db0ac69ef8d9d812cce7cb
Dr. Taintlove <sirwilliamwood@gmail.com>
2023-01-23 19:08:14 -0600

    added js conversion function

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-// placeholder
+
+let item1 = 143.28;
+let item2 = 0.79;
+
+function priceCompare() {
+    let y, x, h;
+    y = item1;
+    h = item2;
+    if(y>h){
+        x = (y/h);
+    } else if(h>y){
+        x = (h/y);
+    } else {
+        console.log("They b equal yipoee");
+    }
+    
+    console.log(Math.round(100 * x)/100);
+};
+
+priceCompare();

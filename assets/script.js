877aa045493bef317dfd18793fdbb423a0e04115
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-01-23 20:49:39 -0600

    jscommit2

--- assets/script.js
+++ assets/script.js
@@ -6,24 +6,35 @@ let willSpoonApi = `eff0c27f45264abbb0afaccac9b87b3a`
 let adamSpoonApi = `4173694ca49d4d7498d18a6a3b6883fd`
 
             function getProductId() {
-                fetch(`https://api.spoonacular.com/food/products/search?query=beans&number=10&apiKey=` + kyleSpoonApi)
+                fetch(`https://api.spoonacular.com/food/products/search?query=beans&number=1&apiKey=` + kyleSpoonApi)
                 .then (function (response) {
                     response.json().then(function (data) {
                         const pId = data.products[0].id
                         console.log(pId);
+                        getProductPrice(pId)
                     })
                 })
             }
 
+            function getProductPrice (pId) {
+                fetch(`https://api.spoonacular.com/food/products/`+pId + `/?apiKey=`+ adamSpoonApi)
+                .then(function (response) {
+                    response.json().then(function (data) {
+                        const productPrice = data.price;
+                        console.log(productPrice);
+                        priceCompare(productPrice);
+                    })
+                })
+            }
             getProductId()
 
 //test compare function
 let item1 = 143.28;
-let item2 = 0.79;
+let item2 = 5.00;
 
-function priceCompare() {
+function priceCompare(productPrice) {
     let y, x, h;
-    y = item1;
+    y = productPrice;
     h = item2;
     if(y>h){
         x = (y/h);
